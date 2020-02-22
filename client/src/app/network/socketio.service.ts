0626b2357a22de07a21fe560da4da935a75b7eb9
Brandon Gardner <brandon.gardner2@outlook.com>
2020-02-22 15:01:26 -0600

    fixes return type for socket io client

--- client/src/app/network/socketio.service.ts
+++ client/src/app/network/socketio.service.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const startSocketIOService = (url: string) => {
+const startSocketIOService = (url: string): SocketIOClient.Socket => {
   return io(url);
 };
 

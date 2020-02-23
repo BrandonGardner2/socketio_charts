f80a7d6bcf2dbb9ffcada3967bdfa4a7f53401c9
Brandon Gardner <brandon.gardner2@outlook.com>
2020-02-23 15:41:49 -0600

    fixes console warnings in header.test

--- client/src/app/components/header/__tests__/Header.test.tsx
+++ client/src/app/components/header/__tests__/Header.test.tsx
@@ -1,5 +1,5 @@
-import React, { ReactComponentElement, ReactElement } from 'react';
-import { render, fireEvent, RenderResult } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
 import Header from '../Header';

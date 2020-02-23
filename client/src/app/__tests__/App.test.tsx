b6eb11f4595d0083f711416d08186bcc8fea4a3f
Brandon Gardner <brandon.gardner2@outlook.com>
2020-02-23 15:41:03 -0600

    beginning to write some tests. found issues with recharts library that should be addressed for node env and also need to look into TS for RTL setup funcs

---
+++ client/src/app/__tests__/App.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import store from '../store/store';
+import App from '../App';
+import { Provider } from 'react-redux';
+
+// There is a issue with Recharts not recognizing responsive container
+// height in a Node environment. I am spying on console warn just to
+// beat it. Planning to look into bypassing this later, as GitHub / SO
+// didn't provide many answers.
+const consoleSpy = jest
+  .spyOn(global.console, 'warn')
+  .mockImplementation(() => null);
+
+test('it renders with the default redux store', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  consoleSpy.mockRestore();
+});
---
+++ client/src/app/components/header/__tests__/Header.test.tsx
@@ -0,0 +1,44 @@
+import React, { ReactComponentElement, ReactElement } from 'react';
+import { render, fireEvent, RenderResult } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import Header from '../Header';
+import store from '../../../store/store';
+
+describe('Header tests', () => {
+  test("it should render with Corva's logo", () => {
+    // In practice this would probably be a variable from somewhere else.
+    const corvaUrl =
+      'https://www.corva.ai/wp-content/themes/corva/framework/dist/images/corva-logo.png';
+    const { container } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    const img = container.querySelector('img');
+    expect(img).toHaveAttribute('src', corvaUrl);
+  });
+
+  test('it should render with my information', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    getByText(/brandon gardner - take home challenge/i);
+  });
+
+  // I am doing more testing of threshold in threshold.test
+  // I can simply check that anything from threshold exists here.
+  test('it should render the threshold component', () => {
+    const { getAllByText } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    expect(getAllByText(/threshold/i).length).toBeGreaterThan(0);
+  });
+});
